56402a5eac1d9695792cda2e3aa53160bf4f107f
Zolootooy_dev <mh2@albedo.dev>
2020-07-29 17:27:17 +0300

    Added CountriesSeeder, countries.json

---
+++ src/database/seeds/CountriesSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Country;
+
+class CountriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('countries')->delete();
+        $json = File::get("database/data/countries.json");
+        $data = json_decode($json);
+
+        foreach ($data as $obj){
+            Country::create(array(
+                'id' => $obj->id,
+                'name' => $obj->name
+            ));
+        }
+    }
+}
