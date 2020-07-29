5a3d8362d7b52222a7fc8e3cb26b5418a4ecc39f
Zolootooy_dev <mh2@albedo.dev>
2020-07-29 17:21:43 +0300

    Added countries, person migrations

---
+++ src/database/migrations/2020_07_29_124243_create_people_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname',255);
+            $table->string('lastname',255);
+            $table->date('birthdate');
+            $table->string('rep_subject',255);
+
+            $table->foreignId('country_id');
+            $table->foreign('country_id')->references('id')->on('countries');
+            $table->string('phone', 50);
+            $table->string('email', 70)->unique();
+            $table->string('company', 70)->nullable();
+            $table->string('position', 100)->nullable();
+            $table->text('about')->nullable();
+            $table->string('photo', 255)->nullable();
+
+            $table->boolean('show');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
